6a7eaf7855bbb1c8ae364a877e910c66344c9aa5
rediet <rb.rediet@gmail.com>
2025-07-18 21:50:25 +0300

    feat: configure font loader and apply font style

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
