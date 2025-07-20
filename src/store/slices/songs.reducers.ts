849a09b5959ab0f89496d0b64589c1b9fc12da40
rediet <rb.rediet@gmail.com>
2025-07-20 22:36:20 +0300

    refactor: modularize song reducers into separate functions for better maintainability

---
+++ src/store/slices/songs.reducers.ts
@@ -0,0 +1,156 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import { Song, SongsData, UpdateSongPayload, CreateSongPayload } from "@/types";
+import { SongState } from "@/store/slices/songsSlice";
+import { updateToastToError, updateToastToSuccess } from "@/utils";
+
+export const songGetReducers = {
+  getSongs: (state: SongState) => {
+    state.status.getSongs = "loading";
+    state.errors.getSongs = null;
+  },
+  getSongsSuccess: (state: SongState, action: PayloadAction<SongsData>) => {
+    state.songs = action.payload.songs;
+    state.total = action.payload.total;
+    state.status.getSongs = "success";
+  },
+  getSongsFailure: (state: SongState, action: PayloadAction<string>) => {
+    state.status.getSongs = "error";
+    state.errors.getSongs = action.payload;
+  },
+
+  getSong: (state: SongState, action: PayloadAction<{ id: string }>) => {
+    state.status.getSong = "loading";
+    state.errors.getSong = null;
+  },
+  getSongSuccess: (state: SongState, action: PayloadAction<Song>) => {
+    state.song = action.payload;
+    state.status.getSong = "success";
+  },
+  getSongFailure: (state: SongState, action: PayloadAction<string>) => {
+    state.status.getSong = "error";
+    state.errors.getSong = action.payload;
+  },
+};
+
+export const songCreateReducers = {
+  createSong: (state: SongState, action: PayloadAction<CreateSongPayload>) => {
+    state.status.createSong = "loading";
+    state.errors.createSong = null;
+  },
+  createSongSuccess: (
+    state: SongState,
+    {
+      payload: { newSong, toastId },
+    }: PayloadAction<{ newSong: Song; toastId?: string | number }>
+  ) => {
+    state.songs.push(newSong);
+    state.total += 1;
+    state.status.createSong = "success";
+
+    updateToastToSuccess("Song created successfully", toastId);
+  },
+  createSongFailure: (
+    state: SongState,
+    {
+      payload: { message, toastId },
+    }: PayloadAction<{ message: string; toastId?: string | number }>
+  ) => {
+    state.status.createSong = "error";
+    state.errors.createSong = message;
+
+    updateToastToError(message, toastId);
+  },
+};
+
+export const songUpdateReducers = {
+  updateSong: (state: SongState, action: PayloadAction<UpdateSongPayload>) => {
+    state.status.updateSong = "loading";
+    state.errors.updateSong = null;
+  },
+  updateSongSuccess: (
+    state: SongState,
+    {
+      payload: { updatedSong, toastId },
+    }: PayloadAction<{ updatedSong: Song; toastId?: string | number }>
+  ) => {
+    state.songs = state.songs.map((song) =>
+      song.id === updatedSong.id ? updatedSong : song
+    );
+    if (state.song && state.song.id === updatedSong.id) {
+      state.song = updatedSong;
+    }
+    state.status.updateSong = "success";
+
+    updateToastToSuccess("Song updated successfully", toastId);
+  },
+  updateSongFailure: (
+    state: SongState,
+    {
+      payload: { message, toastId },
+    }: PayloadAction<{ message: string; toastId?: string | number }>
+  ) => {
+    state.status.updateSong = "error";
+    state.errors.updateSong = message;
+
+    updateToastToError(message, toastId);
+  },
+};
+
+export const songDeleteReducers = {
+  deleteSong: (state: SongState, action: PayloadAction<{ id: string }>) => {
+    state.status.deleteSong = "loading";
+    state.errors.deleteSong = null;
+  },
+  deleteSongSuccess: (
+    state: SongState,
+    {
+      payload: { id, toastId },
+    }: PayloadAction<{ id: string; toastId?: string | number }>
+  ) => {
+    state.songs = state.songs.filter((song) => song.id !== id);
+    state.total -= 1;
+    state.status.deleteSong = "success";
+
+    updateToastToSuccess("Song deleted successfully", toastId);
+  },
+  deleteSongFailure: (
+    state: SongState,
+    {
+      payload: { message, toastId },
+    }: PayloadAction<{ message: string; toastId?: string | number }>
+  ) => {
+    state.status.deleteSong = "error";
+    state.errors.deleteSong = message;
+
+    updateToastToError(message, toastId);
+  },
+};
+
+export const pageParamReducers = {
+    changePage: (state: SongState, { payload: page }: PayloadAction<number>) => {
+        if (page < 1) {
+          console.error("Page number cannot be less than 1. Defaulting to 1.");
+          page = 1;
+        }
+  
+        state.page = page;
+        if (state.status.getSongs !== "loading") {
+          state.status.getSongs = "idle";
+          state.errors.getSongs = null;
+        }
+      },
+      changePerPage: (state: SongState, { payload: perPage }: PayloadAction<number>) => {
+        if (perPage < 1) {
+          console.error(
+            "Items per page cannot be less than 1. Defaulting to 10."
+          );
+          perPage = 10;
+        }
+  
+        state.perPage = perPage;
+        if (state.status.getSongs !== "loading") {
+          state.status.getSongs = "idle";
+          state.errors.getSongs = null;
+        }
+      },
+}
--- src/store/slices/songsSlice.ts
+++ src/store/slices/songsSlice.ts
@@ -1,8 +1,12 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
-import { CreateSongPayload, Song, SongsData, UpdateSongPayload } from "@/types";
-import { toast } from "sonner";
-import { updateToastToError, updateToastToSuccess } from "@/utils";
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  pageParamReducers,
+  songCreateReducers,
+  songDeleteReducers,
+  songGetReducers,
+  songUpdateReducers,
+} from "./songs.reducers";
+import { Song } from "@/types";
 
 type SongOperation =
   | "getSongs"
@@ -17,7 +21,7 @@ type SongStatus = Record<
 >;
 type SongErrors = Record<SongOperation, string | null>;
 
-interface SongState {
+export interface SongState {
   songs: Song[];
   total: number;
   page?: number;
@@ -30,8 +34,6 @@ interface SongState {
 const initialState: SongState = {
   songs: [],
   total: 0,
-  page: undefined,
-  perPage: undefined,
   song: null,
   status: {
     getSongs: "idle",
@@ -53,147 +55,11 @@ const songsSlice = createSlice({
   name: "songs",
   initialState,
   reducers: {
-    getSongs: (state) => {
-      state.status.getSongs = "loading";
-      state.errors.getSongs = null;
-    },
-    getSongsSuccess: (state, action: PayloadAction<SongsData>) => {
-      state.songs = action.payload.songs;
-      state.total = action.payload.total;
-      state.status.getSongs = "success";
-    },
-    getSongsFailure: (state, action: PayloadAction<string>) => {
-      state.status.getSongs = "error";
-      state.errors.getSongs = action.payload;
-    },
-
-    getSong: (state, action: PayloadAction<{ id: string }>) => {
-      state.status.getSong = "loading";
-      state.errors.getSong = null;
-    },
-    getSongSuccess: (state, action: PayloadAction<Song>) => {
-      state.song = action.payload;
-      state.status.getSong = "success";
-    },
-    getSongFailure: (state, action: PayloadAction<string>) => {
-      state.status.getSong = "error";
-      state.errors.getSong = action.payload;
-    },
-
-    createSong: (state, action: PayloadAction<CreateSongPayload>) => {
-      state.status.createSong = "loading";
-      state.errors.createSong = null;
-    },
-    createSongSuccess: (
-      state,
-      {
-        payload: { newSong, toastId },
-      }: PayloadAction<{ newSong: Song; toastId?: string | number }>
-    ) => {
-      state.songs.push(newSong);
-      state.total += 1;
-      state.status.createSong = "success";
-
-      updateToastToSuccess("Song created successfully", toastId);
-    },
-    createSongFailure: (
-      state,
-      {
-        payload: { message, toastId },
-      }: PayloadAction<{ message: string; toastId?: string | number }>
-    ) => {
-      state.status.createSong = "error";
-      state.errors.createSong = message;
-
-      updateToastToError(message, toastId);
-    },
-
-    updateSong: (state, action: PayloadAction<UpdateSongPayload>) => {
-      state.status.updateSong = "loading";
-      state.errors.updateSong = null;
-    },
-    updateSongSuccess: (
-      state,
-      {
-        payload: { updatedSong, toastId },
-      }: PayloadAction<{ updatedSong: Song; toastId?: string | number }>
-    ) => {
-      state.songs = state.songs.map((song) =>
-        song.id === updatedSong.id ? updatedSong : song
-      );
-      if (state.song && state.song.id === updatedSong.id) {
-        state.song = updatedSong;
-      }
-      state.status.updateSong = "success";
-
-      updateToastToSuccess("Song updated successfully", toastId);
-    },
-    updateSongFailure: (
-      state,
-      {
-        payload: { message, toastId },
-      }: PayloadAction<{ message: string; toastId?: string | number }>
-    ) => {
-      state.status.updateSong = "error";
-      state.errors.updateSong = message;
-
-      updateToastToError(message, toastId);
-    },
-
-    deleteSong: (state, action: PayloadAction<{ id: string }>) => {
-      state.status.deleteSong = "loading";
-      state.errors.deleteSong = null;
-    },
-    deleteSongSuccess: (
-      state,
-      {
-        payload: { id, toastId },
-      }: PayloadAction<{ id: string; toastId?: string | number }>
-    ) => {
-      state.songs = state.songs.filter((song) => song.id !== id);
-      state.total -= 1;
-      state.status.deleteSong = "success";
-
-      updateToastToSuccess("Song deleted successfully", toastId);
-    },
-    deleteSongFailure: (
-      state,
-      {
-        payload: { message, toastId },
-      }: PayloadAction<{ message: string; toastId?: string | number }>
-    ) => {
-      state.status.deleteSong = "error";
-      state.errors.deleteSong = message;
-
-      updateToastToError(message, toastId);
-    },
-
-    changePage: (state, { payload: page }: PayloadAction<number>) => {
-      if (page < 1) {
-        console.error("Page number cannot be less than 1. Defaulting to 1.");
-        page = 1;
-      }
-
-      state.page = page;
-      if (state.status.getSongs !== "loading") {
-        state.status.getSongs = "idle";
-        state.errors.getSongs = null;
-      }
-    },
-    changePerPage: (state, { payload: perPage }: PayloadAction<number>) => {
-      if (perPage < 1) {
-        console.error(
-          "Items per page cannot be less than 1. Defaulting to 10."
-        );
-        perPage = 10;
-      }
-
-      state.perPage = perPage;
-      if (state.status.getSongs !== "loading") {
-        state.status.getSongs = "idle";
-        state.errors.getSongs = null;
-      }
-    },
+    ...songGetReducers,
+    ...songCreateReducers,
+    ...songUpdateReducers,
+    ...songDeleteReducers,
+    ...pageParamReducers,
   },
 });
 
